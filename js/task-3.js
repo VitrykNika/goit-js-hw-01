0aa30567f8472395711112654de25848e48d5b71
Vitryk Nika <v.karpinska@ukr.net>
2025-05-31 20:40:09 +0300

    new files update

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+    'use strict';
+    
+    function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+  
+    const totalWidth = contentWidth + paddingValue * 2 + borderValue * 2;
+    return totalWidth;
+}
+
+    console.log(getElementWidth("50px", "8px", "4px")); 
+    console.log(getElementWidth("60px", "12px", "8.5px")); 
+    console.log(getElementWidth("200px", "0px", "0px")); 
+
+
+
